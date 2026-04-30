0c136dbdac4ad29a634ea35bac8c5a42ce5f6a28
Shawn-Ramcharan <shawn.ramcharan@gmail.com>
2026-04-30 16:50:31 -0400

    alias SQLModel.Session as DbSession to avoid conflict with model session

--- src/afd_dbserver/models/__init__.py
+++ src/afd_dbserver/models/__init__.py
@@ -1,19 +1,17 @@
 from sqlmodel import (
     SQLModel,
-    Session,
     create_engine
 )
+from sqlmodel import Session as DbSession
 from .project import Project
 from .location import Location
 from .appliance import Appliance
 from .physical_asset import PhysicalAsset
+from .session import Session
 
 engine = create_engine("sqlite:///./afd_local.db", echo=True)
 SQLModel.metadata.create_all(engine)
 
 def get_session():
-    with Session(engine) as session:
-        yield session
-
-
-
+    with DbSession(engine) as dbsession:
+        yield dbsession
